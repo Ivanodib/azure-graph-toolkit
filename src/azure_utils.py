a151a97fc4dc42c2dbe247a961f976dd0e152837
ivano.dibenedetto <ivano.dibenedetto7@gmail.com>
2024-08-03 17:39:51 +0200

    Added function to get user group

--- src/azure_utils.py
+++ src/azure_utils.py
@@ -33,3 +33,42 @@ def get_user_id_from_upn (UPN, access_token ):
             'error':error_message
         }
 
+def get_user_group_by_name (user_id,group_name,access_token):
+
+    url = f'https://graph.microsoft.com/v1.0/users/{user_id}/memberOf/microsoft.graph.group'
+
+    params = {
+        '$count':'true',
+        '$search':f'"displayName:{group_name}"',
+        '$select':'displayName,id'
+    }
+
+    headers = {
+        'Authorization':f'Bearer {access_token}',
+        'Content-type':'application/json',
+        'ConsistencyLevel':'eventual'
+    }
+
+    try:
+        result = requests.get(url,headers=headers, params=params)
+        result.raise_for_status()
+        groups_data = result.json()
+
+        for group in groups_data['value']:
+            if group_name in group['displayName']:
+                return {
+                    'status_code':result.status_code,
+                    'aad_group_id':group['id'],
+                    'aad_group_name':group['displayName']
+                }
+        return {
+            'status_code':result.status_code,
+            'error':f'No AAD group that contains {group_name} for user {user_id} found. Try another name'
+        }
+    except requests.exceptions.HTTPError as e: 
+        error_message = e.response.json().get('error', {}).get('message',str(e))
+        logging.error(f'Error: {error_message}.')
+        return {
+            'status_code': e.response.status_code,
+            'error':error_message
+        }
