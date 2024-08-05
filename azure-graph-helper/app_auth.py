c4d2facdb9d7f393bb7352d9e1c3b0353cef74f1
ivano.dibenedetto <ivano.dibenedetto7@gmail.com>
2024-08-05 22:52:05 +0200

    refactoring

--- azure-graph-helper/app_auth.py
+++ azure-graph-helper/app_auth.py
@@ -1,19 +1,6 @@
 from msal import ConfidentialClientApplication
-from msal import SerializableTokenCache
 import config
 import os
-
-
-def load_cache():
-    cache = SerializableTokenCache()
-    if os.path.exists('/src/token_cache.json'):
-        cache.deserialize(open('/src/token_cache.json', 'r').read())
-    return cache
-
-def save_cache(cache):
-    if cache.has_state_changed:
-        open('token_cache.json', 'w').write(cache.serialize())
-
 
 
 def get_access_token(tenant_id, client_id, client_secret):
@@ -21,14 +8,11 @@ def get_access_token(tenant_id, client_id, client_secret):
     auth_url = f'{config.AUTH_BASE_URL}/{tenant_id}'
     scopes = ['https://graph.microsoft.com/.default']
 
-    cache = load_cache()
-
     app = ConfidentialClientApplication(
 
         client_id=client_id,
         client_credential=client_secret,
-        authority=auth_url,
-        token_cache=cache
+        authority=auth_url
     )
     
     result = app.acquire_token_for_client(scopes=scopes)
