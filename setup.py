004e82872db50886547808f9766183e2918e35e7
ivano.dibenedetto <ivano.dibenedetto7@gmail.com>
2024-08-13 14:29:12 +0200

    setup deployment

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="azure-graph-toolkit",
-    version="1.0.0",
+    version="1.0",
     author="Ivano Dibenedetto",
     author_email="ivano.dibenedetto7@gmail.com",
     description="Python library for easily managing Azure AD (Entra ID) users and groups through the Graph API.",
